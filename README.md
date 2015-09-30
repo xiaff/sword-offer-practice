1a574d259fe009ffc6d1328d768aeebb2231e4f1
夏方方 <loochenwei@gmail.com>
2015-09-30 18:48:53 +0800

    :rocket: Add Problem3

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 
 ## [面试题2：实现Singleton模式](https://github.com/xiaff/sword-offer-practice/blob/master/problem_2.cpp)
 设计一个类，我们只能生成该类的一个实例。
+
+## [面试题3：二维数组中的查找](https://github.com/xiaff/sword-offer-practice/blob/master/problem_3.cpp)
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
---
+++ problem_3.cpp
@@ -0,0 +1,39 @@
+/*
+面试题3：二维数组中的查找
+在一个二维数组中，每一行都按照从左到右递增的顺序排序，
+每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+*/
+
+#include<vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+	bool findInPartiallySortedMatrix(vector<vector<int>> matrix, int number) {
+		int rowNumber = matrix.size();
+		if (rowNumber == 0) {
+			return false;
+		}
+		int colNumber = matrix[0].size();
+		if (colNumber == 0) {
+			return false;
+		}
+
+		for (int row = 0, col = colNumber - 1;row<rowNumber&&col >= 0;;) {
+			int current = matrix[row][col];
+			if (number == current) {
+				return true;
+			}
+			else if (number<current) {
+				col--;
+			}
+			else {
+				row++;
+			}
+		}
+		return false;
+	}
+};
